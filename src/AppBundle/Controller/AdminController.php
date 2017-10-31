d746519805d77ec649205ee5efce37dadcc02e66
Kyle Minshall <kyleminshall@gmail.com>
2017-10-30 22:33:50 -0700

    Adding changes to allow admins to be added via web tool

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * @Route("/admin")
@@ -17,10 +18,10 @@ class AdminController extends Controller
     public function adminHomeAction(Request $request)
     {
         return $this->render('admin/index.html.twig', [
-            
+
         ]);
     }
-    
+
     /**
      * @Route("/users", name="admin_users")
      */
@@ -29,17 +30,60 @@ class AdminController extends Controller
         $em = $this->get('doctrine')->getManager();
         $users = $em->getRepository('AppBundle\Entity\User')
                     ->findAll();
-        
+
         return $this->render('admin/users.html.twig', [
             'users' => $users,
         ]);
     }
-    
+
     /**
      * @Route("/users/make_admin", name="admin_make_admin")
      */
     public function adminMakeAdminAction(Request $request)
     {
+      $em = $this->get('doctrine')->getManager();
+      $user_id = $request->request->get('user_id');
+      $user = $em->getRepository('AppBundle\Entity\User')->find($user_id);
 
+      if (!$user) {
+        return new JsonResponse([
+          'msg' => 'Could not locate user with the given id',
+        ], 400);
+      }
+
+      $user_name = $user->getName();
+      $user->setRole('ROLE_ADMIN');
+      $em->persist($user);
+      $em->flush();
+
+      return new JsonResponse([
+        'msg' => "Added $user_name as admin.",
+      ]);
+    }
+
+    /**
+     * @Route("/users/revoke_admin", name="admin_revoke_admin")
+     */
+    public function adminRevokeAdminAction(Request $request)
+    {
+
+      $em = $this->get('doctrine')->getManager();
+      $user_id = $request->request->get('user_id');
+      $user = $em->getRepository('AppBundle\Entity\User')->find($user_id);
+
+      if (!$user) {
+        return new JsonResponse([
+          'msg' => 'Could not locate user with the given id',
+        ], 400);
+      }
+
+      $user_name = $user->getName();
+      $user->setRole('ROLE_USER');
+      $em->persist($user);
+      $em->flush();
+
+      return new JsonResponse([
+        'msg' => "Revoked admin rights from $user_name.",
+      ]);
     }
 }
